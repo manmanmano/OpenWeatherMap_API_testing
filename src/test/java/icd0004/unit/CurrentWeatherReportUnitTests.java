0596a8a5a1a6bb925c26ce24731ef9839eb19bf6
Mihkel Kiil <mikiil@ttu.ee>
2022-11-19 12:34:04 +0200

    PART 1: test for temperature in current weather report

---
+++ src/test/java/icd0004/unit/CurrentWeatherReportUnitTests.java
@@ -0,0 +1,36 @@
+package icd0004.unit;
+
+import icd0004.api.WeatherApi;
+import icd0004.api.dto.CurrentWeatherDto;
+import icd0004.api.dto.MainDto;
+import icd0004.handler.CurrentWeatherHandler;
+import icd0004.report.CurrentWeatherReport;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CurrentWeatherReportUnitTests {
+    @Mock
+    private WeatherApi weatherApiMock;
+
+    @Test
+    public void givenTemp_whenGetCurrentWeatherReport_thenReportShouldContainTemp(){
+        Double temp = -5.0;
+        CurrentWeatherDto weatherDtoStub = new CurrentWeatherDto();
+        weatherDtoStub.setCity(anyString());
+        MainDto mainDtoStub = new MainDto();
+        mainDtoStub.setTemp(temp);
+        weatherDtoStub.setMain(mainDtoStub);
+        when(weatherApiMock.getCurrentWeatherData("Tallinn")).thenReturn(weatherDtoStub);
+        CurrentWeatherHandler currentWeatherHandler = new CurrentWeatherHandler(weatherApiMock);
+        CurrentWeatherReport currentWeatherReport = currentWeatherHandler.getCurrentWeatherReport(anyString());
+
+        assertThat(currentWeatherReport.getTemperature()).isEqualTo(temp);
+    }
+}
