fce7c80f1fd4b0fbe06dee8fd5002cd5edb83c0d
Mariano D'Angelo <madang@taltech.ee>
2022-11-19 11:08:23 +0200

    PART 1: Add constructors to CurrentWeatherHandler

--- src/main/java/icd0004/handler/CurrentWeatherHandler.java
+++ src/main/java/icd0004/handler/CurrentWeatherHandler.java
@@ -1,4 +1,16 @@
 package icd0004.handler;
 
+import icd0004.api.WeatherApi;
+
 public class CurrentWeatherHandler {
+
+    private final WeatherApi weatherApi;
+
+    public CurrentWeatherHandler() {
+        this.weatherApi = new WeatherApi();
+    }
+
+    public CurrentWeatherHandler(WeatherApi weatherApi) {
+        this.weatherApi = weatherApi;
+    }
 }
