fc1d2bd552e6cffa83ed02e768c7762dec88bc55
Mariano D'Angelo <madang@taltech.ee>
2022-11-19 11:01:31 +0200

    PART 1: Add constants with key and url and empty method

--- src/main/java/icd0004/api/WeatherApi.java
+++ src/main/java/icd0004/api/WeatherApi.java
@@ -1,4 +1,12 @@
 package icd0004.api;
 
+import icd0004.api.dto.CurrentWeatherDto;
+
 public class WeatherApi {
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
+    private static final String API_KEY = "f5fed9c8b74174a5c021904ade8402a5";
+
+    public CurrentWeatherDto getCurrentWeatherData() {
+
+    }
 }
