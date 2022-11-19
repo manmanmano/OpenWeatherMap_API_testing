1749a3ca0a17b4df902ea7ad680440cff1e93cb9
Mariano D'Angelo <madang@taltech.ee>
2022-11-19 12:51:13 +0200

    PART 1: Remove unneeded test, edit second unit test

--- src/test/java/icd0004/unit/MainDetailsUnitTests.java
+++ src/test/java/icd0004/unit/MainDetailsUnitTests.java
@@ -1,11 +1,9 @@
 package icd0004.unit;
 
 import icd0004.api.WeatherApi;
+import icd0004.api.dto.CoordinatesDto;
 import icd0004.api.dto.CurrentWeatherDto;
-import icd0004.api.dto.MainDto;
-import icd0004.handler.CurrentWeatherHandler;
 import icd0004.handler.MainDetailsHandler;
-import icd0004.report.CurrentWeatherReport;
 import icd0004.report.MainDetails;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,7 +25,6 @@ public class MainDetailsUnitTests {
         String city = "Keila";
         CurrentWeatherDto weatherDtoStub = new CurrentWeatherDto();
         weatherDtoStub.setCity("Keila");
-        weatherDtoStub.setMain(new MainDto());
         when(weatherApiMock.getCurrentWeatherData(anyString())).thenReturn(weatherDtoStub);
         MainDetailsHandler mainDetailsHandler = new MainDetailsHandler(weatherApiMock);
 
@@ -39,9 +36,11 @@ public class MainDetailsUnitTests {
     @Test
     public void givenCity_whenGetMainDetails_thenReportShouldContainCoordinates(){
         String city = "Tallinn";
+        CoordinatesDto coordinatesDto = new CoordinatesDto();
+        coordinatesDto.setLat(59.44);
+        coordinatesDto.setLon(24.75);
         CurrentWeatherDto weatherDtoStub = new CurrentWeatherDto();
-        weatherDtoStub.setCity("Tallinn");
-        weatherDtoStub.setMain(new MainDto());
+        weatherDtoStub.setCoordinates(coordinatesDto);
         when(weatherApiMock.getCurrentWeatherData(anyString())).thenReturn(weatherDtoStub);
         MainDetailsHandler mainDetailsHandler = new MainDetailsHandler(weatherApiMock);
 
@@ -50,17 +49,4 @@ public class MainDetailsUnitTests {
         assertThat(mainDetails.getCoordinates()).isEqualTo("59.44,24.75");
     }
 
-    @Test
-    public void givenCity_whenGetMainDetails_thenReportShouldContainUnit(){
-        String city = "Paide";
-        CurrentWeatherDto weatherDtoStub = new CurrentWeatherDto();
-        weatherDtoStub.setCity("Paide");
-        weatherDtoStub.setMain(new MainDto());
-        when(weatherApiMock.getCurrentWeatherData(anyString())).thenReturn(weatherDtoStub);
-        MainDetailsHandler mainDetailsHandler = new MainDetailsHandler(weatherApiMock);
-
-        MainDetails mainDetails = mainDetailsHandler.getMainDetails(city);
-
-        assertThat(mainDetails.getTemperatureUnit()).isEqualTo("Celsius");
-    }
 }
