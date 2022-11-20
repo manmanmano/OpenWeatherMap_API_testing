9635185ca156ea05d34642e94ab82d30602c1cec
Mihkel Kiil <mikiil@ttu.ee>
2022-11-20 08:28:11 +0200

    PART 1: add main

--- src/main/java/icd0004/Main.java
+++ src/main/java/icd0004/Main.java
@@ -1,9 +1,20 @@
 package icd0004;
 
+import icd0004.handler.WeatherHandler;
+import icd0004.report.Weather;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String city = args[0];
-        System.out.println();
+        WeatherHandler weatherHandler = new WeatherHandler();
+        Weather weather = weatherHandler.getWeather(city);
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(weather);
+        System.out.println(json);
     }
 }
