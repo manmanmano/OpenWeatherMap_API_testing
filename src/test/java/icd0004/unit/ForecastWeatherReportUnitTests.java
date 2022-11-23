95c4c4055610db99b0925d283028cc3ac4484dbd
Mariano D'Angelo <madang@taltech.ee>
2022-11-23 11:31:17 +0200

    PART 2: Add empty tests

--- src/test/java/icd0004/unit/ForecastWeatherReportUnitTests.java
+++ src/test/java/icd0004/unit/ForecastWeatherReportUnitTests.java
@@ -1,2 +1,34 @@
-package icd0004.unit;public class ForecastWeatherReportUnitTests {
+package icd0004.unit;
+
+import icd0004.api.WeatherApi;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ForecastWeatherReportUnitTests {
+
+    @Mock
+    private WeatherApi weatherApiMock;
+
+    @Test
+    public void givenCity_whenForecastReport_thenReportShouldContainDate() {
+
+    }
+
+    @Test
+    public void givenCity_whenForecastReport_thenReportShouldContainTemperature() {
+
+    }
+
+    @Test
+    public void givenCity_whenForecastReport_thenReportShouldContainPressure() {
+
+    }
+
+    @Test
+    public void givenCity_whenForecastReport_thenReportShouldContainHumidity() {
+
+    }
 }
