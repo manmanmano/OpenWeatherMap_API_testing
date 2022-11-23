b6da59b4148f1591f33ab06b948a4daebebdceb8
Mariano D'Angelo <madang@taltech.ee>
2022-11-23 14:13:44 +0200

    Print humidity before pressure

--- src/main/java/icd0004/report/CurrentWeatherReport.java
+++ src/main/java/icd0004/report/CurrentWeatherReport.java
@@ -6,6 +6,6 @@ import lombok.Data;
 public class CurrentWeatherReport {
     private String date;
     private Double temperature;
-    private int pressure;
     private int humidity;
+    private int pressure;
 }
