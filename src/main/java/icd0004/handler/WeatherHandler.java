47e6f179a5499afab0104742f709b70ff5bbbab0
Mihkel Kiil <mikiil@ttu.ee>
2022-12-10 13:46:18 +0200

    PART 4: add logging

--- src/main/java/icd0004/handler/WeatherHandler.java
+++ src/main/java/icd0004/handler/WeatherHandler.java
@@ -2,6 +2,9 @@ package icd0004.handler;
 
 import icd0004.api.WeatherApi;
 import icd0004.report.Weather;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 
 public class WeatherHandler {
     private final WeatherApi weatherApi;
@@ -13,6 +16,7 @@ public class WeatherHandler {
     public WeatherHandler(WeatherApi weatherApi) {
         this.weatherApi = weatherApi;
     }
+    private static final Logger logger = LogManager.getLogger(WeatherHandler.class);
 
     public Weather getWeather(String city){
         Weather weather = new Weather();
@@ -20,6 +24,7 @@ public class WeatherHandler {
         CurrentWeatherHandler currentWeatherHandler = new CurrentWeatherHandler(weatherApi);
         ForecastWeatherHandler forecastWeatherHandler = new ForecastWeatherHandler(weatherApi);
         if(currentWeatherHandler.getCurrentWeatherReport(city) == null){
+            logger.error("City '" + city + "' not found.");
             return null;
         }
         weather.setCurrentWeatherReport(currentWeatherHandler.getCurrentWeatherReport(city));
