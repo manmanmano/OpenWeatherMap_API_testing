9282160e5f9c5cc41426183a37672132dad4bb88
Mihkel Kiil <mikiil@ttu.ee>
2022-12-10 12:34:00 +0200

    PART 4: add message for json writing

--- src/main/java/icd0004/handler/WeatherFileWriter.java
+++ src/main/java/icd0004/handler/WeatherFileWriter.java
@@ -8,7 +8,7 @@ public class WeatherFileWriter {
     public static File writeJsonToFile(String json, String city) throws IOException {
         File weatherFile = new File(city.toLowerCase() + ".json");
         Files.write(weatherFile.toPath(), json.getBytes());
-
+        System.out.println("Weather information written to " + weatherFile.toPath());
         return weatherFile;
     }
 }
